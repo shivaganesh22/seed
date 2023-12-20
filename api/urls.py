3c171c44d63ac5a6237ac733a573f7a898f81502
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2023-12-20 07:00:49 +0000

    movie

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from api.views import *
 urlpatterns = [
-    path('movierulz/',movierulz)
+    path('movierulz/',movierulz),
+    path('movierulz/movie/',movierulzmovie),
 ]
--- api/views.py
+++ api/views.py
@@ -12,3 +12,21 @@ def movierulz(r):
         if not "trailer"  in i.a.get('title').lower():
             movies.append({"name":i.a.get('title'),"link":i.a.get('href'),"image":i.img.get('src')})
     return JsonResponse({"movies":movies})
+def movierulzmovie(r):
+    req=requests.get(r.GET.get('link'))
+    soup=bs(req.content,'html.parser')
+    items=soup.findAll('a',class_='mv_button_css')
+    links=[]
+    for i in items:
+        b=i.findAll('small')
+        links.append({"name":b[0].get_text()+" "+b[1].get_text(),"link":i.get('href')})
+    items=soup.findAll('p')
+    details={}
+    details["name"]=soup.find('h2',class_='entry-title').get_text()
+    for i in items:
+        if "directed" in i.get_text().lower():
+            details["inf"]=i.prettify()
+            j=i.find_next_sibling()
+            details["desc"]=j.prettify()
+    details["image"]=soup.find('img',class_='attachment-post-thumbnail').get('src')
+    return JsonResponse({"links":links,"details":details})
