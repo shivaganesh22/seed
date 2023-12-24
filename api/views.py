46377d1c88d7e0bc56593a7810882b7e66b81e6b
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2023-12-24 17:26:04 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -229,8 +229,8 @@ def deletefile(r,id):
     return Response({"status":"false"},status=status.HTTP_401_UNAUTHORIZED)
 
 
-def mainsearch(r,query,no):
-    req=requests.get("https://torrentz2.nz/search?q="+query+"&page="+str(no))
+def mainsearch(r):
+    req=requests.get("https://torrentz2.nz/search?q="+r.GET['q']+"&page="+r.GET['page'])
     soup=bs(req.content,'html.parser')
     title=soup.find('h2').get_text()
     item=soup.find('div',class_='results')
