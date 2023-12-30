5da5f7064f43f51fc78a53bac89d6bb52354d687
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2023-12-30 20:51:56 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -374,7 +374,7 @@ def sports(r):
         item=items[i]
         if (item.href.includes("telegram")) item.remove()
         else if (item.href.includes("cricstreaming"))
-        item.href=item.href
+        item.href="/api/sports/player/?link="+item.href
         
     }
 """
