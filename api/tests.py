aa77be446f200cff5c6dd46f82e437f22c41324b
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2023-12-30 14:21:52 +0000

    A

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+import requests
+from bs4 import BeautifulSoup as bs
+
+req=requests.get("https://sports-cricstreaming.vercel.app")
+soup=bs(req.content,'html.parser')
+items=soup.find('head')
+sc=soup.new_tag('script')
+sc.string="""
+ tt = document.querySelector(".btn-11");
+    tt.remove()
+    items=document.getElementsByTagName("a")
+    for(i=0;i<items.length;i++){
+        item=items[i]
+        if (item.href.includes("telegram")) item.remove()
+        else if (item.href.includes("cricstreaming"))
+        item.href='/sports/player/?link='+item.href
+        
+    }
+"""
+items.insert(0,sc)
+print(items)
--- api/views.py
+++ api/views.py
@@ -364,8 +364,22 @@ def ibommamovie(r):
 def sports(r):
     req=requests.get("https://sports-cricstreaming.vercel.app")
     soup=bs(req.content,'html.parser')
-    items=soup.find('head').prettify()
-    return JsonResponse({"items":items})
+    items=soup.find('head')
+    sc=soup.new_tag('script')
+    sc.string="""
+ tt = document.querySelector(".btn-11");
+    tt.remove()
+    items=document.getElementsByTagName("a")
+    for(i=0;i<items.length;i++){
+        item=items[i]
+        if (item.href.includes("telegram")) item.remove()
+        else if (item.href.includes("cricstreaming"))
+        item.href='/sports/player/?link='+item.href
+        
+    }
+"""
+    items.insert(0,sc)
+    return JsonResponse({"items":items.prettify()})
 def sportsplayer(r):
     req=requests.get(r.GET['link'])
     soup=bs(req.content,'html.parser')
