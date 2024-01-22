ff96056d6361cccb151367ca52333176eed277c3
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-01-22 19:33:42 +0530

    logo

--- seed/settings.py
+++ seed/settings.py
@@ -126,8 +126,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+if DEBUG:
+
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT=os.path.join(BASE_DIR,"static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- seed/urls.py
+++ seed/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.views.static import serve
+
 from app.views import *
 from api import urls
 urlpatterns = [
@@ -56,9 +59,10 @@ urlpatterns = [
     path('test/',test),
     path('api/data/',apidata),
 
-    path('api/',include(urls.urlpatterns))
+    path('api/',include(urls.urlpatterns)),
 
 
-
+re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
