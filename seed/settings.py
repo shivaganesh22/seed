7af72533f8f53746dac438363c5b0079d2c08639
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-02-18 16:47:00 +0530

    cors

--- seed/settings.py
+++ seed/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    "corsheaders",
     'mathfilters',
     'api',
     
@@ -47,10 +48,17 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    
+    "http://localhost:3080",
+    "http://192.168.187.208:3000",
 ]
 
 ROOT_URLCONF = 'seed.urls'
