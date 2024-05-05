4b5928826b988693ef4f5a36d06b5654f37cb4c4
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2024-05-05 17:29:54 +0000

    fix

--- app/admin.py
+++ app/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 admin.site.register(Contact)
+admin.site.register(Movierulz)
+admin.site.register(IBomma)
+admin.site.register(FCM_token)
--- app/models.py
+++ app/models.py
@@ -12,10 +12,14 @@ class Movierulz(models.Model):
     name=models.CharField(max_length=150)
     image=models.URLField()
     link=models.URLField()
+    def __str__(self):
+        return self.name
 class IBomma(models.Model):
     name=models.CharField(max_length=150)
     image=models.URLField()
     link=models.URLField()
+    def __str__(self):
+        return self.name
 class FCM_token(models.Model):
     token=models.CharField(max_length=500,unique=True)
     added_at = models.DateTimeField(auto_now_add=True)
