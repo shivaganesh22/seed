2588cbe0929b6d505e9534b037ffee1a661bf1ad
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2024-06-07 09:58:00 +0530

    Update all_movies.py

--- react/all_movies.py
+++ react/all_movies.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup as bs
 
 #home
-domain="https://movierulzhd.green/"
+domain="https://movierulzhd.rocks/"
 def allMovies(r):
     req=requests.get(domain+"movierulz-2024-watch-free-movies-online-hindi-telugu-tamil-english/")
     soup=bs(req.content,'html.parser')
